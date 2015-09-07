9640cc4cb88e82552152c3686fa4d0e4a0213808
FrannyZhao <franny8912@gmail.com>
2015-09-07 16:37:54 +0800

    JiuZhang Algorithm Chapter One Solutions of subSets
    Three solutions of subSets http://www.lintcode.com/en/problem/subsets/;
    One solution of subSets of list that has duplicate numbers: http://www.lintcode.com/en/problem/subsets-ii/.

---
+++ src/AlgorithmSolutions/JiuZhangChapterOneSolutions.java
@@ -0,0 +1,127 @@
+package AlgorithmSolutions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class JiuZhangChapterOneSolutions {
+
+	/*
+	 *  use recursion
+	 */
+	public ArrayList<ArrayList<Integer>> subSets (ArrayList<Integer> S) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if (S != null) {
+			Collections.sort(S);
+			subsetsHelper(result, list, S, 0);
+//			subsetsHelper2(result, S);
+			return result;
+		} else {
+			return null;
+		}
+	}
+	
+	/* take [0, 1, 4] for example
+	 *                    []
+	 *         --------------------------
+	 *         |             |          |
+	 *        [0]           [1]        [4]
+	 *     ---------         |
+	 *     |       |         |
+	 *   [0, 1]  [0, 4]    [1, 4]   
+	 *     |   
+	 * [0, 1, 4]   
+	 *        
+	 *  put element into the list one by one
+	 */
+	private void subsetsHelper(ArrayList<ArrayList<Integer>> result, 
+			ArrayList<Integer> list, ArrayList<Integer> S, int pos) {
+		result.add(new ArrayList<Integer>(list));
+		System.out.println("--START-- pos = "+pos+", result = "+result);
+		for (int i = pos; i < S.size(); i++) {
+			list.add(S.get(i));
+			System.out.println("i = "+i+", list = "+list);
+			subsetsHelper(result, list, S, i+1);
+			list.remove(list.size() - 1);
+			System.out.println("i = "+i+", pos = "+pos+", list.remove = "+list);
+		}
+		System.out.println("--END-- pos = "+pos);
+	}
+	
+	/*
+	 * take [0, 1, 4] for example
+	 * minimum the problem by see what is the result of [], it is [[]]
+	 * Then add one element [0], the result is []'s result plus [0], it is [[], [0]]
+	 * Then add one more element [0, 1], the result is [0]'s result plus [1] to each element, []+[1] and [0]+[1], it is [[], [0], [1], [0, 1]]
+	 * So we can get the conclusion is that suppose we have n element ArrayList, the result is 
+	 * (n-1)'s result + {add n to each element of (n-1)'s result}
+	 */
+	private void subsetsHelper2(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> S) {
+		if (S.size() == 0) {
+			result.add(new ArrayList<Integer>(S));
+		} else {
+			int lastElement = S.get(S.size()-1);
+			ArrayList<Integer> STmp = new ArrayList<Integer>(S);
+			STmp.remove(STmp.size()-1);
+			subsetsHelper2(result, STmp);
+			ArrayList<ArrayList<Integer>> resultTmp = new ArrayList<ArrayList<Integer>>(result);
+			ArrayList<Integer> list;
+			for (int j = 0; j < resultTmp.size(); j ++) {
+				list = new ArrayList<Integer>(resultTmp.get(j));
+				list.add(lastElement);
+				result.add(new ArrayList<Integer>(list));
+			}
+		}
+	}
+
+	/*
+	 * do not use recursion, use bitmap
+	 */
+	public ArrayList<ArrayList<Integer>> subSets2 (ArrayList<Integer> S) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int size = S.size();
+		Collections.sort(S);
+		if (size > 0) {
+			char[] maxValue = new char[size];
+			for (int i = 0; i < maxValue.length; i++) {
+				maxValue[i] = '1';
+			}
+			int value = Integer.parseInt(String.valueOf(maxValue), 2);
+			for (int i = 0; i <= value; i ++) {
+				String s = Integer.toBinaryString(i);
+				char[] c = s.toCharArray();
+				for (int j = 0; j < c.length; j++) {
+					if (c[j] != '0') {
+						list.add(S.get(size-(c.length-j)));
+					}
+				}
+				result.add(new ArrayList<Integer>(list));
+				list.clear();
+			}
+		}
+		return result;
+	}
+	
+	public ArrayList<ArrayList<Integer>> subsetsWithDup(ArrayList<Integer> S) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		Collections.sort(S);
+		subsetsWithDupHelper(result, list, S, 0);
+		return result;
+	}
+	
+	private void subsetsWithDupHelper(ArrayList<ArrayList<Integer>> result, 
+			ArrayList<Integer> list, ArrayList<Integer> S, int pos) {
+		result.add(new ArrayList<Integer>(list));
+		for (int i = pos; i < S.size(); i++) {
+			if (i != pos && S.get(i-1) == S.get(i)) {
+				continue;
+			}
+			list.add(S.get(i));
+			subsetsWithDupHelper(result, list, S, i+1);
+			list.remove(list.size()-1);
+		}
+	}
+	
+}
---
+++ src/AlgorithmSolutions/JiuZhangChapterOneTest.java
@@ -0,0 +1,35 @@
+package AlgorithmSolutions;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Collections;
+
+public class JiuZhangChapterOneTest {
+	
+	private static JiuZhangChapterOneSolutions solution = new JiuZhangChapterOneSolutions();
+	private static ArrayList<Integer> S = new ArrayList<Integer>();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		S.add(4); S.add(1); S.add(0); S.add(0);
+		/*
+		for (int i = 0; i < 3; i++) {
+			S.add(i);
+		}
+		long start = System.currentTimeMillis();
+		System.out.println("start = "+start);
+		System.out.println(solution.subSets(S));
+		long end = System.currentTimeMillis();
+		System.out.println("end = "+end);
+		System.out.println("Spend time 1: "+(end-start)+" ms");
+		*/
+		/*
+		start = System.currentTimeMillis();
+		System.out.println(solution.subSets2(S));
+		end = System.currentTimeMillis();
+		System.out.println("Spend time 2: "+(end-start)+" ms");
+		*/
+		System.out.println(solution.subsetsWithDup(S));
+	}
+
+}
